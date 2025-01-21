d33d5752f502defb2d7b85eeb63ce43d1172f217
Marco <marcoreinoso19@hotmail.com>
2025-01-21 18:25:45 -0500

    Initial commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const store = useStore()
+</script>
+<template>
+  <header>
+    <h1 class="bg-red-100">Vite + Vue 3 + TypeScript + Tailwind + Playwright Starter Template v{{ store.appMeta.version }}</h1>
+  </header>
+
+  <main>
+  </main>
+
+  <footer>
+  </footer>
+</template>
