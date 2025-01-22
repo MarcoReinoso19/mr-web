88d348547831f69058a557c94f9a57304207a743
Marco <marcoreinoso19@hotmail.com>
2025-01-22 15:18:20 -0500

    refactor: update PostCSS configuration and dependencies for Tailwind CSS integration

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,10 +1,6 @@
 /* eslint-env node */
-module.exports = {
+export default {
   plugins: {
-    'postcss-import': {},
-    'tailwindcss/nesting': 'postcss-nesting',
-    tailwindcss: {},
-    autoprefixer: {},
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
+    '@tailwindcss/postcss': {},
   },
 }
